122c24db9597f11aa9ceeb4b3ee0c7f55fbf1a0a
Lynn Wu <wuxinyuexy0104@163.com>
2024-01-19 17:58:52 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// ElementUI的全局注册
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
